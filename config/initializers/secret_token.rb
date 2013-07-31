ac488581d15ca4a7c442d47cbaf2fe2d3821673a
Everton Maldonado <everton.maldonado@gmail.com>
2013-07-30 22:28:17 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RelogioFerias::Application.config.secret_token = '950c4beac1fb0e19cc251eee035d640cb56223d2e6a5d7fd5347b4f351a31cac7d5bc6be980770da28c6f274aa62efd113f5fc15209dca8417e6c08928c40eed'
