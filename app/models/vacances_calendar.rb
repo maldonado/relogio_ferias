9ad5e4f39660ec6747036b56b78a5049f0afc522
Everton Maldonado <everton.maldonado@gmail.com>
2013-07-31 02:20:31 -0300

    add calculation of days

--- app/models/vacances_calendar.rb
+++ app/models/vacances_calendar.rb
@@ -1,3 +1,9 @@
 class VacancesCalendar < ActiveRecord::Base
-  attr_accessible :days_until_vacances
+  
+  # attr_accessible :days_until_vacances
+  
+  def initialize()
+    @days_until_vacances = (Date.new(2012,9,19) - Date.today).to_i
+  end
+  
 end
