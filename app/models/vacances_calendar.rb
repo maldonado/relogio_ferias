34027f959ab6171b9482882a862449879835f555
Everton Maldonado <everton.maldonado@gmail.com>
2013-08-05 01:56:17 -0300

    fix bugs in previus commit

--- app/models/vacances_calendar.rb
+++ app/models/vacances_calendar.rb
@@ -4,20 +4,20 @@ class VacancesCalendar
   
   def initialize()
     set_amount_of_seconds
+    start_count_down
   end
   
   def set_amount_of_seconds
     @seconds = Time.utc(2013, 9, 19, 18, 00, 00).to_i - Time.now.to_i
   end
   
-  def start_cont_down
-    while @seconds > 0
+  def start_count_down
+    # while @seconds > 0
       calculate_seconds
       calculate_minutes
-      calcalate_hours
+      calculate_hours
       calculate_days
-      sleep(1)
-    end
+    # end
   end
       
   def calculate_seconds
@@ -29,7 +29,7 @@ class VacancesCalendar
   end
   
   def calculate_hours
-    @hours = minutes /60
+    @hours = @minutes /60
   end
   
   def calculate_days
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 RelogioFerias::Application.routes.draw do
   match '/' => 'vacances_calendars#show'
+  match '/' => 'vacances_calendars#show', :as => 'atualizar'
 end
