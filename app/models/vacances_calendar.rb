afafca79f708848e5bf60ed00c426fe7fc3de885
Everton Maldonado <everton.maldonado@gmail.com>
2013-08-05 01:10:44 -0300

    add new countdown features to calendar

--- app/models/vacances_calendar.rb
+++ app/models/vacances_calendar.rb
@@ -1,10 +1,39 @@
 class VacancesCalendar
   
-  attr_accessor :days_until_vacances
+  attr_accessor :days, :hours, :minutes, :seconds
   
   def initialize()
-    number_of_days = (Date.new(2013,9,19) - Date.today).to_i
-    @days_until_vacances = "Faltam apenas " + number_of_days.to_s + " dias para as férias do evermale do gabes."
+    set_amount_of_seconds
+  end
+  
+  def set_amount_of_seconds
+    @seconds = Time.utc(2013, 9, 19, 18, 00, 00).to_i - Time.now.to_i
+  end
+  
+  def start_cont_down
+    while @seconds > 0
+      calculate_seconds
+      calculate_minutes
+      calcalate_hours
+      calculate_days
+      sleep(1)
+    end
+  end
+      
+  def calculate_seconds
+    set_amount_of_seconds
+  end
+  
+  def calculate_minutes
+    @minutes = @seconds /60
+  end
+  
+  def calculate_hours
+    @hours = minutes /60
+  end
+  
+  def calculate_days
+    @days = @hours /24
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 RelogioFerias::Application.routes.draw do
-  
-  get 'vacances_calendars', to: 'vacances_calendars#show'
-  get '/', to: 'vacances_calendars#show'
-  
+  match '/' => 'vacances_calendars#show'
 end
